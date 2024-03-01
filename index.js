985af8902591046c015e7fd6ebe4740985195ecc
K.T <z1022001@gmail.com>
2024-03-01 22:11:28 +0800

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+let server = null;
+
+app.all('/hello/', (req, res) => {
+    res.write('hello');
+    res.end();
+})
+
+app.all('/exit/', (req, res) => {
+    res.write('exit');
+    res.end();
+    server.close(() => { console.log("HTTP Server is offline!") });
+})
+
+
+
+const port = process.env.PORT || 8080;
+server = app.listen(port, () => {
+    let nowDate = new Date(Date.now()).toLocaleString('en-ZA', { timeZone: 'Asia/Taipei' });
+    console.log(`HTTP Server is online, port: ${port}! ${nowDate}`);
+});
+
+
